20d8bb222f509eaec379d978857bf6f42d17a2a4
Danilo Leal <daniloleal09@gmail.com>
2025-01-19 14:23:45 -0300

    Fix missing `useCallback` import

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import Image from "next/image";
 import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
 
