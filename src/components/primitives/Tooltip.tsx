7a13680d97222eaacfd353c934ed2020f845b912
zanivan <victorzanivan@gmail.com>
2025-01-21 08:28:32 -0300

    Fix tooltip

--- src/components/primitives/Tooltip.tsx
+++ src/components/primitives/Tooltip.tsx
@@ -11,11 +11,11 @@ function ArrowSvg(props: React.ComponentProps<"svg">) {
       />
       <path
         d="M8.99542 1.85876C9.75604 1.17425 10.9106 1.17422 11.6713 1.85878L16.5281 6.22989C17.0789 6.72568 17.7938 7.00001 18.5349 7.00001L15.89 7L11.0023 2.60207C10.622 2.2598 10.0447 2.2598 9.66436 2.60207L4.77734 7L2.13171 7.00001C2.87284 7.00001 3.58774 6.72568 4.13861 6.22989L8.99542 1.85876Z"
-        className="bg-zinc-50 dark:bg-zinc-900 fill-zinc-200/60 dark:fill-none"
+        className="bg-zinc-50 dark:bg-zinc-900 fill-none dark:fill-none"
       />
       <path
         d="M10.3333 3.34539L5.47654 7.71648C4.55842 8.54279 3.36693 9 2.13172 9H0V8H2.13172C3.11989 8 4.07308 7.63423 4.80758 6.97318L9.66437 2.60207C10.0447 2.25979 10.622 2.2598 11.0023 2.60207L15.8591 6.97318C16.5936 7.63423 17.5468 8 18.5349 8H20V9H18.5349C17.2998 9 16.1083 8.54278 15.1901 7.71648L10.3333 3.34539Z"
-        className=" bg-zinc-50 dark:bg-zinc-900 dark:fill-zinc-600/20"
+        className=" bg-zinc-50 fill-zinc-200 dark:bg-zinc-900 dark:fill-zinc-800"
       />
     </svg>
   );
@@ -43,7 +43,7 @@ export function Tooltip({
                 "bg-zinc-50 dark:bg-zinc-900",
                 "border default-border-color",
                 "[z-index:101]",
-                "origin-[var(--transform-origin)] transition-[transform,scale,opacity] data-[ending-style]:scale-90 data-[ending-style]:opacity-0 data-[instant]:duration-0 data-[starting-style]:scale-90 data-[starting-style]:opacity-0",
+                "origin-[var(--transform-origin)] transition-[transform,scale,opacity] data-[ending-style]:scale-90 data-[ending-style]:opacity-0 data-[instant]:duration-0 data-[starting-style]:scale-90 data-[starting-style]:opacity-0"
               )}
             >
               <BaseTooltip.Arrow className="data-[side=bottom]:top-[-8px] data-[side=left]:right-[-13px] data-[side=left]:rotate-90 data-[side=right]:left-[-13px] data-[side=right]:-rotate-90 data-[side=top]:bottom-[-8px] data-[side=top]:rotate-180">
