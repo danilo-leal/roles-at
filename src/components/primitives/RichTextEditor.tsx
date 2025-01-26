7cead7a598e64496db96bd6f327e0d2e6605bd90
Danilo Leal <daniloleal09@gmail.com>
2025-01-26 18:58:46 -0300

    Fix Tiptap console warning

--- src/components/primitives/RichTextEditor.tsx
+++ src/components/primitives/RichTextEditor.tsx
@@ -25,6 +25,7 @@ export function RichTextEditor({
   const editor = useEditor({
     extensions: [StarterKit],
     content: value,
+    immediatelyRender: false,
     onUpdate: ({ editor }) => {
       onChange(editor.getHTML());
     },
